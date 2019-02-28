bb930174491df475d75cb7b2320d321531938925
Qiang Liu <31175402+liuliu408@users.noreply.github.com>
2019-02-28 19:27:43 +0800

    Update dataset_image2txt.py

--- dataset_image2txt.py
+++ dataset_image2txt.py
@@ -15,3 +15,24 @@ for file in train_files:
 
 for file in test_files:
     test_file.write(file.split('.')[0] + '\n')
+
+---------------------------------------------------------------------------------------------------
+#制作txt文件
+# -*- coding: utf-8 -*-
+# @Author: liuqiang
+# @Date:   2019-02-27 18:10:53
+
+import os
+train_file = open('train.txt', 'w')   #指定train.txt文件路径
+test_file  = open('test.txt', 'w')    #指定test.txt文件路径
+
+for _, _, train_files in os.walk('train_dataset'):  #指定train_dataset训练图片路径
+    continue
+for _, _, test_files in os.walk('test_dataset'):    #指定test_dataset测试图片路径
+    continue
+
+for file in train_files:
+    train_file.write(file.split('.')[0] + '\n')
+
+for file in test_files:
+    test_file.write(file.split('.')[0] + '\n')
